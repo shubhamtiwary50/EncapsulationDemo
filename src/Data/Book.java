36f872b993591d6ad9413354292ade98b93e972e
SHUBHAM <shubhamtiwary50@gmail.com>
2022-08-16 12:25:29 +0530

    Made some necessary package changes

---
+++ src/Data/Book.java
@@ -0,0 +1,17 @@
+package Data;
+
+public class Book {
+        int ISBNNo;
+        String bookName;
+        String author;
+        String genre;
+        float price;
+        public Book(int ISBNNo, String bookName, String author, String genre, float price)
+        {
+            this.ISBNNo=ISBNNo;
+            this.bookName=bookName;
+            this.price=price;
+            this.genre=genre;
+            this.author=author;
+        }
+    }
--- src/Main/BookImpl.java
+++ src/Main/BookImpl.java
@@ -1,4 +1,6 @@
-public class EncapDemo {
+package Main;
+
+public class BookImpl {
     public static void main(String[] args) {
 
     }
